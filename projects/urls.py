82f2c39c146d29220e97bf2eef1dcbb29c874d15
Jesus Ramos <elyisusrh448@gmail.com>
2025-01-25 12:02:53 -0400

    add createView and image

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from projects.views import ListProjects,DetailProjects
+from projects.views import ListProjects,DetailProjects,CreatePageViews
 
 urlpatterns=[
 
-    path("",ListProjects.as_view()),
-    path("detail/<int:pk>",DetailProjects.as_view(),name="detail")
+    path("",ListProjects.as_view(),name="home"),
+    path("detail/<int:pk>",DetailProjects.as_view(),name="detail"),
+    path("create",CreatePageViews.as_view(),name="Create")
 ]
