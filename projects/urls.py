7513a66d9e3733bb97d02e92bb640489355368b5
Jesus Ramos <elyisusrh448@gmail.com>
2025-02-01 10:49:00 -0400

    add deleteview

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from projects.views import ListProjects,DetailProjects,CreatePageViews,UpdatePageView
+from projects.views import ListProjects,DetailProjects,CreatePageViews,UpdatePageView,DeletePageView
 
 
 urlpatterns=[
@@ -8,6 +8,7 @@ urlpatterns=[
     path("",ListProjects.as_view(),name="home"),
     path("detail/<int:pk>",DetailProjects.as_view(),name="detail"),
     path("create",CreatePageViews.as_view(),name="Create"),
-    path("detail/<int:pk>/update",UpdatePageView.as_view(),name="update")
+    path("detail/<int:pk>/update",UpdatePageView.as_view(),name="update"),
+    path("detail/<int:pk>/delete",DeletePageView.as_view(),name="delete")
     
 ]
