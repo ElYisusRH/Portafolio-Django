996da6a0e1db4dbbc623e56af7933c3beabaf9ae
Jesus Ramos <elyisusrh448@gmail.com>
2025-02-15 11:09:49 -0400

    add welcome html

--- projects/urls.py
+++ projects/urls.py
@@ -1,11 +1,13 @@
 
 from django.urls import path
-from projects.views import ListProjects,DetailProjects,CreatePageViews,UpdatePageView,DeletePageView
+from projects.views import *
 
 
 urlpatterns=[
 
-    path("",ListProjects.as_view(),name="home"),
+    path("",WelcomePage,name="welcome"),
+
+    path("list",ListProjects.as_view(),name="home"),
     path("detail/<int:pk>",DetailProjects.as_view(),name="detail"),
     path("create",CreatePageViews.as_view(),name="Create"),
     path("detail/<int:pk>/update",UpdatePageView.as_view(),name="update"),
