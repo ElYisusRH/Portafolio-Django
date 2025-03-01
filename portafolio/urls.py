bef7083601a55f2db10d2612c8f86d9fa2af736c
Jesus Ramos <elyisusrh448@gmail.com>
2025-03-01 09:34:52 -0400

    requirements

--- portafolio/urls.py
+++ portafolio/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects', include('projects.urls')),
+    path('', include('projects.urls')),
     path('auth',include('authentication.urls'))
 ]
 urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
